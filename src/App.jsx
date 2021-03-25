c1c758c7ac911847a273481ad95b3e9de6837114
MakarandPundlik <mmp2308@gmail.com>
2021-03-25 18:51:26 +0530

    layout completed

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React,{useEffect, useState} from 'react';
+import Form from './components/Form';
+import './App.css';
+function App() {
+
+  const getDefaultTheme=()=> {
+    const selectedTheme = JSON.parse(localStorage.getItem('dark'))
+    return selectedTheme || false
+  }
+  const [darkTheme,setDarkTheme] = useState(getDefaultTheme());
+
+  useEffect(()=>{
+    localStorage.setItem('dark',JSON.stringify(darkTheme));
+  },[darkTheme]);
+
+  
+  return (
+    <div className={darkTheme?"dark-theme":"light-theme"}>
+      
+     <nav>
+      
+         <div  onClick={()=>setDarkTheme(prevTheme=>!prevTheme)}>
+           {darkTheme?
+           <ion-icon name="sunny-sharp" size="large"></ion-icon>
+           :<ion-icon size="large" name="moon-sharp"></ion-icon>}
+         </div>
+       
+     </nav>
+      <div className="content">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
