8b8ab370bf43a662687d2c07bed84d31bc5ae07c
MakarandPundlik <mmp2308@gmail.com>
2021-03-25 15:18:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react';
 
+import './App.css';
 function App() {
+
+  const getDefaultTheme=()=> {
+    const selectedTheme = JSON.parse(localStorage.getItem('dark'))
+    return selectedTheme || false
+  }
+  const [darkTheme,setDarkTheme] = useState(getDefaultTheme());
+
+  useEffect(()=>{
+    localStorage.setItem('dark',JSON.stringify(darkTheme));
+  },[darkTheme]);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkTheme?"dark-theme":"light-theme"}>
+      
+     <nav>
+      
+         <div  onClick={()=>setDarkTheme(prevTheme=>!prevTheme)}>
+           {darkTheme?<ion-icon name="sunny-sharp" size="large"></ion-icon>:<ion-icon size="large" name="moon-sharp"></ion-icon>}
+         </div>
+       
+     </nav>
+      <div className="content">
+      <h1>
+       {darkTheme?"Dark":"Light"} Theme
+       </h1>
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Nav(props) {
+    return (
+        <div className="navbar">
+          Fucking navbar
+          </div>
+    );
+}
+
+
+export default Nav;
