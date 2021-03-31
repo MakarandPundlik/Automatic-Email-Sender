93ff19b3f80964b3a69ea77369828ddcfb6a6fe5
MakarandPundlik <mmp2308@gmail.com>
2021-03-31 23:07:08 +0530

    now with files

--- server.js
+++ server.js
@@ -18,24 +18,14 @@ app.use(bodyparser.urlencoded({
 app.use(bodyparser.json());
 
 const port = process.env.port||3001;
-app.listen(port,()=>console.log(`listening to the port ${port}...`));
-
-//setting up the nodemailer
-const contactEmail = nodemailer.createTransport({
-    service:'gmail',
-    host:"blogpost.example.com",
-    port:465,
-    secure:true,
-   auth:{
-      user:"",
-      pass:""
-   }
+app.listen(port,()=>{
+    console.log(`listening to the port ${port}...`)
+    
 });
 
-contactEmail.verify((error)=>{
-    if(!error) console.log("Ready to send...");
-    else console.log(error);
-});
+
+
+
 
 
 //listen to the request
@@ -55,21 +45,49 @@ router.use((req,res,next)=>{
 
 router.post("/contact",(req,res)=>{
     const {email,name} = req.body;
-    console.log(req.body);
+   // console.log(req.body);
     const mail = {
         from:"blogpost.example@gmail.com",
         to:email,
         subject:"Welcome to BlogPost community ",
-        html:`<p>Welcome to BlogPost community <h3>${name}</h3></p>`
+        html:`<p>Welcome to BlogPost community <h3>${name}</h3>This mail has attachments</p>`,
+        attachments:[
+            {
+                filename:'students.xls',
+                path:'F:/PICT/TE/students.xlsx'
+            }
+        ]
     }
+
+    //setting up the nodemailer
+   
+   
+const contactEmail = nodemailer.createTransport({
+    service:'gmail',
+    host:"blogpost.example.com",
+    port:465,
+    secure:true,
+   auth:{
+      user:process.env.email,
+      pass:process.env.password
+   }
+});
+
+contactEmail.verify((error)=>{
+    if(!error) console.log("Ready to send...");
+    else console.log(error);
+});
 
     contactEmail.sendMail(mail,(err)=>{
         if(!err) {
-            console.log("Email sent successfully...");
+        console.log("An email has been sent...");
            // console.log(process.env.user,process.env.pass);
-            return res.json({name,email});
+            return res.json({msg:`Email sent successfully to ${email}`});
         }
-        else console.log(err);
+        else {
+            console.log(err);
+            return res.json({status:"Unable to send mail..."})
+        }
     })
     //return res.json({msg:"something went wrong"});
 });
